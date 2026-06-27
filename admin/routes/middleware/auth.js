85df78345b3ff38ed404518828c1b40d3e9b2732
CHUCKY👨‍💻 <128384293+Chanakasl@users.noreply.github.com>
2026-06-27 18:48:09 +0530

    Add JWT authentication middleware

---
+++ admin/routes/middleware/auth.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req,res,next)=>{
+
+const token=req.headers.authorization;
+
+if(!token){
+
+return res.status(401).json({
+
+success:false,
+message:"Unauthorized"
+
+});
+
+}
+
+try{
+
+const decoded=jwt.verify(token.replace("Bearer ",""),process.env.JWT_SECRET);
+
+req.admin=decoded;
+
+next();
+
+}catch(err){
+
+return res.status(401).json({
+
+success:false,
+message:"Invalid Token"
+
+});
+
+}
+
+}
